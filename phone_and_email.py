5a6b1a4f396e91c3a925186a4d13fc22b19ae68a
Emre Ertem <emre.ertem123@gmail.com>
2021-07-29 20:02:07 +0200

    Completed regex for phone numbers and email adresses

--- phone_and_email.py
+++ phone_and_email.py
@@ -4,10 +4,24 @@
 import pyperclip
 import re
 
+# Create phone regex
 phone_regex = re.compile(r"""(
-    (\d{3}|\(\d{3}\))?          # area code
-    (\s|-|\.)?                  # sperator 
-    (\d{3})                     # first 3 digits
-    (\s|-|\.)                  # sperator
-    (\d{4})                     # first 4 digits
+    (\d{3}|\(\d{3}\))?              # area code
+    (\s|-|\.)?                      # sperator 
+    (\d{3})                         # first 3 digits
+    (\s|-|\.)                       # sperator
+    (\d{4})                         # first 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
 )""", re.VERBOSE)
+
+# Create email regex
+email_regex = re.compile(r"""(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # @ symbol
+    [a-zA-Z0-9.-]+                  # domain name
+    (\.[a-zA-Z]{2,4})               # dot-something                                
+)""", re.VERBOSE)
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
