933e9bf66f714296cb031f98d90e9ba31df2c7e4
Emre Ertem <emre.ertem123@gmail.com>
2021-07-29 21:09:45 +0200

    find matches in clipboard text and copy results

--- phone_and_email.py
+++ phone_and_email.py
@@ -7,7 +7,7 @@ import re
 # Create phone regex
 phone_regex = re.compile(r"""(
     (\d{3}|\(\d{3}\))?              # area code
-    (\s|-|\.)?                      # sperator 
+    (\s|-|\.)?                      # sperator
     (\d{3})                         # first 3 digits
     (\s|-|\.)                       # sperator
     (\d{4})                         # first 4 digits
@@ -19,9 +19,25 @@ email_regex = re.compile(r"""(
     [a-zA-Z0-9._%+-]+               # username
     @                               # @ symbol
     [a-zA-Z0-9.-]+                  # domain name
-    (\.[a-zA-Z]{2,4})               # dot-something                                
+    (\.[a-zA-Z]{2,4})               # dot-something
 )""", re.VERBOSE)
 
-# TODO: Find matches in clipboard text.
+# Find matches in clipboard text.
+text = str(pyperclip.paste())
 
-# TODO: Copy results to the clipboard.
+matches = []
+for groups in phone_regex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+    for groups in email_regex.findall(text):
+        matches.append(groups[0])
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
